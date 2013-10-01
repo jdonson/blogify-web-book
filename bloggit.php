38039228ba09f0435002f0bb193197dab8e225bf
Jeremy Donson <jjdonson@gmail.com>
2013-10-01 08:15:16 -0400

    Added file bloggit.php to get simple page started.

---
+++ bloggit.php
@@ -0,0 +1,39 @@
+=> blogify
+<!DOCTYPE html>
+<html>
+<head lang="en-US">
+<meta charset="UTF-8">
+<title>
+BlogGit Book: Pro Git
+</title>
+<style type="text/css">body, article a#title {color:#413d3d}</style>
+</head>
+<body>
+<ul id="topnav">
+<li>blogify logo</li>
+<li>book</li>
+<li>login</li>
+<li>about blogify</li>
+<li>contact us</li>
+<li>search</li>
+<li>help</li>
+</ul>
+<iframe id="frame_home" src="">
+BOOK TOC + Index + Glossary
+iframe with anchors => http://git-scm.com/book
+</iframe>
+page change history....
+related quotes + links
+instructor questions
+code samples
+discussions
+submit comment
+make new post
+<footer>
+  <p>Posted by: Hege Refsnes</p>
+  <p>Contact information: <a href="mailto:someone@example.com">
+  someone@example.com</a>.</p>
+</footer>
+</body>
+</html>
+
